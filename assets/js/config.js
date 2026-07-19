d8b1a52751d01bd3b3262a4e0536244b945cdc97
Ben Hur <benhurfig@gmail.com>
2026-07-19 08:17:29 -0400

    update

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@ const SITE_CONFIG = {
   businessName: "Espaço dos Anjos Child Care",
   phone: "7742328156",
   formattedPhone: "(774) 232-8156",
-  email: "lucianjosribeiro@hotmail.com",
+  email: "espacodosanjos.chilcare@gmail.com",
   city: "Clinton",
   state: "MA",
   hours: "7:30 AM – 5:30 PM",
